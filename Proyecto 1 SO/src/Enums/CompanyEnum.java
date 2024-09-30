5ce1e9d4a04af76a004730b19a74a1dfc2b15da0
eduardo bazzani <vsaldivia@correo.unimet.edu.ve>
2024-09-30 13:33:05 -0400

    Clases Enums ❗️

--- Proyecto 1 SO/src/Enums/CompanyEnum.java
+++ Proyecto 1 SO/src/Enums/CompanyEnum.java
@@ -5,8 +5,20 @@
 package Enums;
 
 /**
- *
+ * Permite identificar a cada compañía
  * @author vickysaldivia
  */
 public enum CompanyEnum {
+    Apple(0),
+    Dell(1);
+    
+    private final int id;
+    
+    private CompanyEnum(int id){
+        this.id = id;
+    }
+    
+    public int getId(){
+        return this.id;
+    }
 }
--- Proyecto 1 SO/src/Enums/WorkerEnum.java
+++ Proyecto 1 SO/src/Enums/WorkerEnum.java
@@ -5,9 +5,42 @@
 package Enums;
 
 /**
- *
+ * Permite identificar a los tipos de trabajadores
  * @author vickysaldivia
  */
 public enum WorkerEnum {
+    /** Productor de Placa Base */
+    PlacaBase(0),
     
+    /** Productor de CPU */
+    Procesador(1),
+    
+    /** Productor de Memoria RAM */
+    MemoriaRAM(2),
+    
+    /** Productor de Fuente de Alimentación */
+    FuenteAlimentacion(3),
+    
+    /** Productor de Tarjeta Gráfica */
+    TarjetaGrafica(4),
+    
+    /** Ensamblan el computador con componentes del almacén */
+    Ensamblador(5),
+    
+    /** Mantiene registro de los días */
+    Manager(6),
+    
+    /** Conoce el deadline y entrega los productos */
+    Director(7);
+    
+    private final int id;
+    
+    /** Permite conocer el id de los tipos de trabajadores*/
+    private WorkerEnum(int id){
+        this.id = id;
+    }
+    
+    public int getId(){
+        return this.id;
+    }
 }
