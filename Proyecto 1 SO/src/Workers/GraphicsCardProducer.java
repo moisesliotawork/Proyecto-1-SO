9a01be8dbc4006aaebb6f5a0dbc09ebaeee07b02
eduardo bazzani <vsaldivia@correo.unimet.edu.ve>
2024-10-10 14:19:51 -0400

    Graphics Card Producer ❗️

--- Proyecto 1 SO/src/Workers/GraphicsCardProducer.java
+++ Proyecto 1 SO/src/Workers/GraphicsCardProducer.java
@@ -4,10 +4,71 @@
  */
 package Workers;
 
+import Enums.WorkerEnum;
+import Rules.CompanyRules;
+import Store.Drive;
+
 /**
  *
  * @author Moises Liota
  */
-public class GraphicsCardProducer {
+public class GraphicsCardProducer extends Worker{
     
+    private final float graphicsCardProductionRate;
+    /**
+     * Constructor de la clase GraphicsCardProducer.
+     * 
+     * @param drive Almacén (Drive) donde se almacenarán las tarjetas gráficas producidas.
+     * @param companyRules Reglas específicas de la compañía.
+     * @param costPerHour Costo por hora del trabajador.
+     */
+    public GraphicsCardProducer(Drive drive, CompanyRules companyRules, float costPerHour) {
+        super(WorkerEnum.GraphicsCardProducer, costPerHour, drive, companyRules);
+        this.graphicsCardProductionRate = companyRules.getGraphicsCardProductionPerDay();
+    }
+
+    /**
+     * Método implementado de Worker para realizar el trabajo del productor de tarjetas gráficas.
+     */
+    @Override
+    public void work() {
+        try {
+            drive.getWarehouse().getProducerSemaphore().acquire();
+
+            drive.getWarehouse().addProduct(graphicsCardProductionRate, WorkerEnum.GraphicsCardProducer);
+            hoursWorked += 24;
+
+            updateCost(24); // Actualiza el costo acumulado del trabajador
+
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } finally {
+            drive.getWarehouse().getProducerSemaphore().release();
+        }
+    }
+
+    /**
+     * Método que se ejecuta cuando se inicia el hilo del trabajador.
+     */
+    @Override
+    public void run() {
+        while (isWorking) {
+            try {
+                work();
+                Thread.sleep(drive.getDayDuration() * 1000);
+
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+    
+    /**
+     * Método para actualizar el costo del trabajador en función de las horas trabajadas.
+     * @param hours Horas trabajadas en el ciclo.
+     */
+    private void updateCost(int hours) {
+        float totalCost = costPerHour * hours;
+        //System.out.println("El costo de producción para " + hours + " horas es: $" + totalCost);
+    }
 }
